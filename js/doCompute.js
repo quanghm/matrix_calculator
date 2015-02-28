472d3ef1aa9b5a73b9f5fa997ca51baf473ad7c0
Quang Hoang <quanghm@gmail.com>
2015-03-01 02:16:28 +0900

    Fix some minor bugs. New slider.

--- js/doCompute.js
+++ js/doCompute.js
@@ -1,12 +1,35 @@
 /**
  * 
  */
+	var slideCount = 0;
+	var slideWidth = 0;
+	var slideHeight = 0;
+	var sliderUIWidth = 0;
+	var currentSlide=1;
+	
 function setDimension(){
+	
 	$("#matrix_entries").empty();
+	
+	//	in case we are working with square matrices
+	if (($("#operation").val()=="findDet")||($("#operation").val()=="inverse"))
+	{
+		$("#num_cols").val($("#num_rows").val());
+		$("#augmented_cols").val("0");
+	}
+	
 	num_rows=$("#num_rows").val();
 	num_cols=$("#num_cols").val();
 	augmented_cols =$("#augmented_cols").val();
 
+	//	validate entries
+	if ((num_rows<=0) ||(num_cols<=0)|| (augmented_cols<0)||(augmented_cols>num_cols))
+		{
+		alert("Invalid dimensions. Please review your input.");
+		return;
+		}
+
+	
 	for (row=1;row<=num_rows;row++)
 	{
 		for (col=1;col<=num_cols;col++)
@@ -71,7 +94,7 @@ $("#frmMatrix").submit(function(event){
 	//	new matrix object
 	objMatrix = new matrix(aMatrix,augmented_cols);
 //	display the matrix in output div
-	writeOutputToElement("original_matrix",objMatrix.displayEntries());		
+//	writeOutputToElement("original_matrix",objMatrix.displayEntries());		
 	//	what operation
 	switch ($("#operation").val())
 	{
@@ -92,42 +115,89 @@ $("#frmMatrix").submit(function(event){
 })
 
 function initSlider(){
-	$("#step1").show();
+	//	Get slider info
+	slideCount = $('#slider div.step').length;
+	slideWidth = $('#slider div.step').width();
+	slideHeight = $('#slider div.step').height();
+	sliderUIWidth = slideCount * slideWidth;
+	currentSlide=1;
+	
+	//	Set Slider width
+	$('#outputDiv').css({ width: slideWidth, height: slideHeight });
+	$("#slider").css({ width: sliderUIWidth, marginLeft:0,left:0});
+	//slideFirst();
+}
+//form control
+function slideLeft(){
+	if (currentSlide<slideCount)
+	{
+		$("#slider").animate({
+			left:"-="+slideWidth
+		})
+		currentSlide++;
+	}
+	
+}
+
+function slideRight(){
+	if (currentSlide>1)
+	{
+		$("#slider").animate({
+			left:"+="+slideWidth
+		})
+		currentSlide--;
+	}
+}
+function slideLast(){
+	$("#slider").animate({
+		left:-sliderUIWidth+slideWidth
+	})
+	currentSlide=slideCount;
+}
+function slideFirst(){
+	$("#slider").animate({
+		left:0
+	})
+	currentSlide=1;
 }
 
 //	Step control form
 //	previous button
 $("#previous").click(function(){
-	var currStep = $("div.step:visible");
-	if (currStep.attr("id")!="step1")
-	{
-		currStep.hide();
-		currStep.prev().show();
-	}
+	slideRight();
+//	var currStep = $("div.step:visible");
+//	if (currStep.attr("id")!="step1")
+//	{
+//		currStep.toggle("slide");
+//		currStep.prev().toggle({direction:"left",easing:"linear"});
+//	}
 });
 
 //	next button
 $("#next").click(function(){
-	var currStep = $("div.step:visible");
-	if (currStep.attr("id")!="computation_result")
-	{
-		currStep.hide();//animate({width:'toggle'},350);
-		currStep.next().show();//animate({width:'toggle'},350);
-	}
+	slideLeft();
+//	var currStep = $("div.step:visible");
+//	if (currStep.attr("id")!="computation_result")
+//	{
+//		currStep.toggle({easing:"linear"});
+//		currStep.next().toggle({easing:"linear"});
+//	}
 });
 
 //	last button
 $("#last").click(function(){
-	var currStep=$("div.step:visible");
-	currStep.hide();
-	$("#computation_result").show();
+	slideLast();
+//	var currStep=$("div.step:visible");
+//	currStep.toggle({easing:"linear"});
+//	$("#computation_result").toggle({easing:"linear"});
 });
 
 //	first button
 $("#first").click(function(){
-	var currStep=$("div.step:visible");
-	currStep.hide();
-	$("#step1").show();
+	slideFirst();
+//	var currStep=$("div.step:visible");
+//	currStep.toggle({easing:"linear"});
+//	$("#step1").toggle({easing:"linear"});
 });
 
 $("#goBack").click(function(event){
@@ -137,4 +207,4 @@ $("#goBack").click(function(event){
 	$("#dimensionForm").show();
 	$("#matrix_entries").hide();
 	$("#computeDiv").hide();
-})
+});
